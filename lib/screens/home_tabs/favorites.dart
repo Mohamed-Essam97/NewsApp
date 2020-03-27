3f764526972258ed80a721c54d2d1b537dc04057
HP <mido.essam2016@hotmail.com>
2020-03-27 07:24:42 +0200

    Drawer(Menu)

--- lib/screens/home_tabs/favorites.dart
+++ lib/screens/home_tabs/favorites.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class Favorites extends StatefulWidget {
@@ -6,10 +8,141 @@ class Favorites extends StatefulWidget {
 }
 
 class _FavoritesState extends State<Favorites> {
+
+
+  List<Color> colorsList =
+  [
+    Colors.red,
+    Colors.teal,
+    Colors.lightGreen,
+    Colors.indigo,
+    Colors.brown,
+    Colors.purple,
+  ];
+
+
+  Random random = Random();
+
+  Color _getRandomColor()
+  {
+
+    return colorsList[random.nextInt(colorsList.length)];
+
+  }
+
+
+
+
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.yellow,
+    return ListView.builder(
+        itemBuilder: (context ,Position){
+           return Card(
+             child: Padding(
+               padding: const EdgeInsets.all(16),
+               child: Container(
+                 child: Column(
+                   children: <Widget>[
+                     _autherRow(),
+                     SizedBox(height: 16,),
+                     _newsItemRow(),
+                   ],
+                 ),
+               ),
+             ),
+           );
+
+
+    },
+    itemCount: 20,
     );
   }
+
+
+  Widget _autherRow()
+  {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Row(
+          children: <Widget>[
+            Container(
+                decoration: BoxDecoration(
+                    image:DecorationImage(image: ExactAssetImage('assets/images/home.png'),
+                    fit: BoxFit.cover,
+                    ),
+                  shape: BoxShape.circle,
+                ),
+              width: 50,
+              height: 50,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left:12),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text("Mohamed Essam",style: TextStyle(color: Colors.grey),),
+                  Padding(
+                    padding: const EdgeInsets.only(top:8.0),
+                    child: Row(
+                      children: <Widget>[
+                        Text('15 Min .',style: TextStyle(color: Colors.grey),),
+                        Text('Life Style',style: TextStyle(color: _getRandomColor()),),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+        IconButton(icon: Icon(Icons.bookmark_border,color: Colors.grey,), onPressed: (){})
+      ],
+    );
+
+  }
+
+  Widget _newsItemRow()
+  {
+    return Row(
+      children: <Widget>[
+        Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(image: ExactAssetImage('assets/images/bg.png'),fit: BoxFit.cover),
+          ),
+          width: 124,
+          height: 100,
+          margin: EdgeInsets.only(right: 16),
+        ),
+        Expanded(
+          child: Column(
+            children: <Widget>[
+              Text('Tech Tent: Old Phone and safe social',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600
+              ),),
+              SizedBox(height: 8,),
+              Text('We also talk about the future of work as the robots advance, and work form home. ',style: TextStyle(
+                color: Colors.grey,
+                fontSize: 16,
+                height: 1,
+              ),),
+            ],
+          ),
+        ),
+      ],
+
+    );
+
+  }
+
+
+
+
+
+
+
+
 }
--- lib/share_ui/navigation_drawer.dart
+++ lib/share_ui/navigation_drawer.dart
@@ -1,12 +1,46 @@
 import 'package:flutter/material.dart';
+
 class NavigationDrawer extends StatefulWidget {
   @override
   _NavigationDrawerState createState() => _NavigationDrawerState();
 }
 
 class _NavigationDrawerState extends State<NavigationDrawer> {
+  List<String> navMenu = [
+    'Explore',
+    'Headline News',
+    'Read Later',
+    'Videos',
+    'Photos',
+    'Settings',
+    'Logout'
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Drawer();
+    return Drawer(
+      child: Padding(
+        padding: EdgeInsets.only(top: 75, left: 32),
+        child: ListView.builder(
+          itemBuilder: (context, position) {
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListTile(
+                title: Text(
+                  navMenu[position],
+                  style: TextStyle(color: Colors.grey.shade700, fontSize: 22),
+                ),
+                trailing: Icon(
+                  Icons.chevron_right,
+                  color: Colors.grey.shade400,
+                ),
+                onTap: () {},
+              ),
+            );
+          },
+          itemCount: navMenu.length,
+        ),
+      ),
+    );
   }
 }
